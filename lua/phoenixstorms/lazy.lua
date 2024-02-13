45a3da859f8a2929e91dcb85340bce376ba9d3ea
alexmurz <alex_murz@icloud.com>
2024-02-13 19:30:51 +0300

    format lazy.lua

--- lua/phoenixstorms/lazy.lua
+++ lua/phoenixstorms/lazy.lua
@@ -1,39 +1,39 @@
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup plugins
 require('lazy').setup({
-	-- Fuzzy search
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		-- or                              , branch = '0.1.x',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	},
-	-- Color theme
-	{
-		'rose-pine/neovim', name = 'rose-pine'
-	},
-	-- Syntax highlight
-	{
-		'nvim-treesitter/nvim-treesitter',
+    -- Fuzzy search
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        -- or                              , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    -- Color theme
+    {
+        'rose-pine/neovim', name = 'rose-pine'
+    },
+    -- Syntax highlight
+    {
+        'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
-	},
-	-- Git TUI
-	{
-		'tpope/vim-fugitive'
-	},
+    },
+    -- Git TUI
+    {
+        'tpope/vim-fugitive'
+    },
     -- Command preview helper
     {
         "folke/which-key.nvim",
@@ -49,12 +49,11 @@ require('lazy').setup({
         },
     },
     -- LSP
-	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-	{'williamboman/mason.nvim'},
-	{'williamboman/mason-lspconfig.nvim'},
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'L3MON4D3/LuaSnip'},
+    { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+    { 'neovim/nvim-lspconfig' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'L3MON4D3/LuaSnip' },
 })
-
